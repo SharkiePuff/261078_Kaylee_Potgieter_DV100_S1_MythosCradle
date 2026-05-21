747c67f99b72cb055cddda5bacc23aef9c359ec9
Kaylee <261078@virtualwindow.co.za>
2026-05-21 15:47:59 +0200

    Input counters fixed

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,24 @@
-let contact = document.forms['contact'];
+//let contact = document.forms['contact'];
 
-contact.addEventListener("submit", getContactInfo);
+//contact.addEventListener("submit", getContactInfo);
 
-function getContactInfo(event)
+//function getContactInfo(event){
 
-event.preventDefault();
+//event.preventDefault();
+//}
 
-let 
+ function decCount(inputId){
+ let count = document.getElementById(inputId).value;
+     if( count> 0){
+    count -- ;}
+    document.getElementById(inputId).value = count;
+
+ }
+  
+function incCount(inputId){
+
+ let count = document.getElementById(inputId).value;
+    count ++ ;
+    document.getElementById(inputId).value = count;
+}
+
