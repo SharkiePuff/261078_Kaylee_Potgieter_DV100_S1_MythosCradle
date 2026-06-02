f09b04d541c4f608f13ebc516297446d1503d7ce
Kaylee <261078@virtualwindow.co.za>
2026-06-02 11:06:21 +0200

    Added java

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,4 @@
-//let contact = document.forms['contact'];
-
-//contact.addEventListener("submit", getContactInfo);
-
-//function getContactInfo(event){
-
-//event.preventDefault();
-//}
+let 
 
  function decCount(inputId){
  let count = document.getElementById(inputId).value;
@@ -24,15 +17,38 @@ function incCount(inputId){
 }
 
 
-//const container = document.getElementById('adoptCard-container');
+let container = document.getElementById('adoptCard-container');
 
-//const card = document.createElement('div');
-//card.classList.add('adoptCartCard');
+let card = document.createElement('div');
+card.classList.add('adoptCartCard');
 
-//card.innerHTML=`
-//<img src="../assets/img/adopt1.png" class = "cartImg">
+card.innerHTML=`
+<img src="${image}" class = "cartImg">
+<div class="cartCardContent">
+
+    <div class="cartContentLeft">
+            <div class="cartName">Blue Dragon – Azuron</div>
+            <div class="cartImg"><img src="../assets/img/adopt1.png"></div>
+            </div>
+
+            <div class="cartContentRight">
+
+                
+            <button class="blueCradle">Delete</button>
+            </div>
 
 
+`;
 
-`
+container.appendChild(card);
 
+//addCradle.addEventListener("addCard", ()
+
+//for(let item of )
+
+//for(let food of favFoods){
+
+        //if (food.checked == true){
+            //formData['fav-foods'].push(food.value);
+        
+    
